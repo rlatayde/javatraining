222c96147157176dbcc885edae9c211eb7b6b0f9
Rowell L. Atayde <rlatayde@headoffice.corp.bpi.com.ph>
2025-12-05 09:14:16 +0800

    initial commit

---
+++ book.java
@@ -0,0 +1,12 @@
+package seatwork1;
+
+public class book {
+String name = "Game of Thrones";
+String author = "George R. R. Martin";
+
+void info() {
+	System.out.println("Book title: " + name);
+	System.out.println("Author: " + author);
+}
+}
+
---
+++ seatwork1main.java
@@ -0,0 +1,11 @@
+package seatwork1;
+
+public class seatwork1main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		book seat1 = new book();
+		seat1.info();
+	}
+
+}
