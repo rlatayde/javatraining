fc7c5bf686a5e31b298ede52dd2ed5c36a3612f1
Rowell L. Atayde <rlatayde@headoffice.corp.bpi.com.ph>
2025-12-18 08:06:56 +0800

    M1_activity5

---
+++ activity5.java
@@ -0,0 +1,18 @@
+package activity5;
+
+public class activity5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		// Loop from 1 to 50
+        for (int i = 1; i <= 50; i++) {
+            sum += i; // Add each number to sum
+        }
+
+        // Print the result
+        System.out.println("The sum of numbers from 1 to 50 is: " + sum);
+	}
+
+}
+
